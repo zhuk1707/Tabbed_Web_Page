c6c376f1c779af40b9a370861fab3aa10280411a
zhuk <charizardik64@gmail.com>
2022-08-07 14:34:26 +0300

    Added horizontal tabs

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+function openTab(event, tabName) {
+  let tabContent, tabLink
+  
+  tabContent = document.getElementsByClassName('tab__content')
+  tabLink = document.getElementsByClassName('tab__link')
+
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none'
+  }
+
+  for (let i = 0; i < tabLink.length; i++) {
+    tabLink[i].className = tabLink[i].className.replace(' active', '')
+  }
+  
+  document.getElementById(tabName).style.display = 'block'
+  event.currentTarget.className += ' active'
+}
+
+document.getElementById('openedByDefault').click()
